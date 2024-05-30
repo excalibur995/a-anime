453cbb8ec9aa9021c37ef7f8faae4acf80ab86fc
excalibur995 <albazzyr@gmail>
2024-05-30 10:37:38 +0700

    fix: add suspense to boundary for useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import AdvanceSearch from "@/modules/anime/components/advance-search-anime";
 import AnimeList from "@/modules/anime/components/anime-list";
 import { AnimeProvider } from "@/modules/anime/contexts/FilterContexts";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 container mx-auto p-4">
-      <AnimeProvider>
-        <AdvanceSearch />
-        <AnimeList />
-      </AnimeProvider>
+      <Suspense>
+        <AnimeProvider>
+          <AdvanceSearch />
+          <AnimeList />
+        </AnimeProvider>
+      </Suspense>
     </main>
   );
 }
